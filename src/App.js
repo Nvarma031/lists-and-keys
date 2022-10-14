3ec5ae7b1018a267711af7a3b8ff37c023b9244e
Naveen varma <nvarma031@gmail.com>
2022-10-14 05:07:02 +0000

    doubt

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from "./components/UserProfile/index"
+
+const userDetails = {
+    imageUrl = "https://assets.ccbp.in/frontend/react-js/devon-lane-img.png",
+    name="Naveen",
+    role="Software developer"
+}
+
+const App = () => <UserProfile/>
+
+export default App
